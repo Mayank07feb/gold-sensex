842475c941682dc7119fdca463404e227aaca72a
Mayank Sharma <makedy07feb@gmail.com>
2024-05-29 13:06:08 +0530

    first commit

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon'); // Updated to target .menu-icon
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
 
-// JavaScript to handle sidebar toggle
-const menuIcon = document.getElementById('menu-icon');
-const sidebar = document.getElementById('sidebar');
-const overlay = document.getElementById('overlay');
+    menuIcon.addEventListener('click', () => {
+        sidebar.classList.toggle('open');
+        overlay.classList.toggle('show');
+    });
 
-menuIcon.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-    overlay.classList.toggle('show');
-});
-
-overlay.addEventListener('click', () => {
-    sidebar.classList.remove('open');
-    overlay.classList.remove('show');
+    overlay.addEventListener('click', () => {
+        sidebar.classList.remove('open');
+        overlay.classList.remove('show');
+    });
 });
